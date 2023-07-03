7674c44fc3a1d9e1b01fee80fb774d93e2e35dfd
Erin Wills <ejw.data@gmail.com>
2023-07-03 13:30:18 -0500

    added html template and css/js support files

---
+++ static/js/tracker.js
@@ -0,0 +1,14 @@
+function trackClick(event) {
+    var elementId = event.target.id;  // Get the ID of the clicked element
+    // Send an AJAX request to the Flask server with the clicked element's ID
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/track_click', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({ 'element_id': elementId }));
+}
+
+// Attach the click event listener to the desired elements on the page
+var elements = document.getElementsByClassName('trackable');
+for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', trackClick);
+}
